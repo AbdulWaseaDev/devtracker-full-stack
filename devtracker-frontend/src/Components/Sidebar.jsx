f88c1247ba1d82d6472da2aaf942429011c82bad
mzeeshan138 <muhammadzeeshan786138@gmail.com>
2025-07-03 15:20:25 +0500

    resolve error

--- devtracker-frontend/src/Components/Sidebar.jsx
+++ devtracker-frontend/src/Components/Sidebar.jsx
@@ -6,7 +6,6 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Offcanvas from "react-bootstrap/Offcanvas";
-import Spinner from "react-bootstrap/Spinner";
 
 export default function OffcanvasExample() {
   const navigate = useNavigate();
